eb0600615191af1c4319a7939f01e1c3ba331365
Mbrowne <brownegm@gmail.com>
2017-09-27 16:24:52 -0700

    Modeling Exercise Update
    Adding more responses

--- ModellingEx.R
+++ ModellingEx.R
@@ -1,1 +1,67 @@
 ###Bootcamp Modeling Exercises###
+
+##Setup statments
+#Clear Workspace
+rm(ls())
+
+## Input data, set parameter values, and/or set intial conditions
+
+##Initialize variable to a vector of NA values
+rmodel<-function(n0,rr,kk,tmax=100, figure=TRUE){
+nn<-matrix(nrow = 1,ncol=(tmax+1)) 
+nn[1]<-n0
+## Perform Calculations
+# Use a loop to iterate the model the desired number of times
+for(ii in 1:tmax){
+nn[ii+1]<-nn[ii]*exp(rr*(1-(nn[ii]/kk)))
+}
+if(figure==TRUE){
+## Display the results by plotting, saving, or showing on-screen.
+plot(1:(tmax+1),nn, type="b", col=4, xlab = "Time", ylab="Population")}
+return(nn)
+}
+
+##Population decreases to n=0
+rmodel(100,-2,600, 100)
+
+## Population approaches stable equilibrium at n*=K, without oscillations
+rmodel(1,1,600)
+
+##Decaying oscillations around n=K
+rmodel(100,.10,50)
+
+## Persistent, regular oscillations
+rmodel(100, 2.5, 50)
+
+## Crazy,random-looking fluctuations
+rmodel(1000, 3, 25, 1500)
+
+##R or the growth rate is the key driver of these patterns.
+
+##Choose six interesting values of this parameter.
+
+par(mfrow=c(3,2), mar=c(4,4,3,2))
+rmodel(100,-2,600, 600)
+rmodel(1,1,600)
+rmodel(100,.10,600)
+rmodel(100, 2.5, 600)
+rmodel(1000, 3, 600)
+rmodel(1994, 1.9, 600)
+
+## d
+nVec<-rmodel(20,1,1000,500)
+which(nVec>=500)[1]
+nVec[1,5]
+
+## e
+tk2<-matrix(nrow = 1,ncol=length(r))
+r<-seq(0.1:0.9, by=0.1)
+for (i in 1:length(r)){ 
+  nVec<-rmodel(20,r[i],1000,500, figure =FALSE)
+  tk2[i]<-which(nVec>=500)[1]
+}
+plot(r,tk2, xlab = "Growth Rate(r)", ylab= expression(t[K/2]))
+print(tk2)
+
+
+
